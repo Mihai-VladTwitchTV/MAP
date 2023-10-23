208426422fc07773a23c1f5e97d07f160bb81d17
Stupy18 <stupariubogdan@gmail.com>
2023-10-23 22:45:10 +0300

    Test

--- src/main/java/map/project/MihaiStupyMAPSpring/baseClasses/FullTimeEmployee.java
+++ src/main/java/map/project/MihaiStupyMAPSpring/baseClasses/FullTimeEmployee.java
@@ -3,10 +3,10 @@ package map.project.MihaiStupyMAPSpring.baseClasses;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
-
 @Data
 @Entity
 @Table(name = "FullTimeEmployee")
+//Test daca merge sa dau pull de la Stupy
 public class FullTimeEmployee extends Employee implements fulltime{
     public FullTimeEmployee() {
     }
