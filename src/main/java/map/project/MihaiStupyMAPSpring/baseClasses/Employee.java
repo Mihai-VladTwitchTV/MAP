9417188524625fec55fe209b739b795816dd095e
Mihai-VladTwitchTV <mitzu.v99444@yahoo.com>
2023-10-23 19:40:20 +0300

    correctly defined tables, added lombok, some issues left to resolve

--- src/main/java/map/project/MihaiStupyMAPSpring/baseClasses/Employee.java
+++ src/main/java/map/project/MihaiStupyMAPSpring/baseClasses/Employee.java
@@ -3,66 +3,48 @@ package map.project.MihaiStupyMAPSpring.baseClasses;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 
 
 @Data
 @Entity
 @Table(name = "Employee")
 public class Employee {
+    @Setter
     @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE)
     @Column(name = "employeeID")
     private int employeeID;
+
+    @Setter
     @Getter
     @Column(name = "firstName")
     private String firstName;
+
+    @Setter
+    @Getter
     @Column(name = "lastName")
     private String lastName;
+
+    @Setter
+    @Getter
     @Column(name = "phoneNumber")
     private int phoneNumber;
+
+    @Setter
+    @Getter
     @Column(name = "emailAdress")
     private int emailAdress;
 
-    @JoinColumn(name = "departmentID")
+    @Setter
     @ManyToOne
+    @JoinColumn(name = "departmentID")
     private Department department;
 
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public int getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public int getEmailAdress() {
-        return emailAdress;
-    }
-
     public int getDepartmentID() {
         return department.getDepartmentID();
-    }
-
-    public void setEmployeeID(int employeeID) {
-        this.employeeID = employeeID;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void setEmailAdress(int emailAdress) {
-        this.emailAdress = emailAdress;
     }
 
     public void setDepartmentID(int departmentID) {
