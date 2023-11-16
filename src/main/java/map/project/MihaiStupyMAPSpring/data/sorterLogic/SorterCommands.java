f0a7e1871cca3f6b7423632ad9ff55f534b6e083
Stupy18 <stupariubogdan@gmail.com>
2023-11-16 18:12:31 +0200

    Remade the Strategy Pattern, just needed a .getDepartment().getDepartmentID() instead of directly accesing DepartmentID from employee (dumb mistake :) )

--- src/main/java/map/project/MihaiStupyMAPSpring/data/sorterLogic/SorterCommands.java
+++ src/main/java/map/project/MihaiStupyMAPSpring/data/sorterLogic/SorterCommands.java
@@ -1,53 +1,53 @@
-//package map.project.MihaiStupyMAPSpring.data.sorterLogic;
-//
-//import map.project.MihaiStupyMAPSpring.data.repository.DepartmentRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.shell.standard.ShellComponent;
-//import org.springframework.shell.standard.ShellMethod;
-//import org.springframework.shell.standard.ShellOption;
-//import org.springframework.stereotype.Component;
-//
-//@ShellComponent
-//@Component
-//public class SorterCommands {
-//    //@Autowired
-//    private Sorter sorter;
-//
-//    @Autowired
-//    private DepartmentSorter depsort;
-//    @Autowired
-//    private EmployeeSorter empsort;
-//
-//
-//   /// @ShellMethod(key="set-repo",value="Set sorting repo")
-//    public void setSortRepo(Sorter sorter){
-//        this.sorter = sorter;
-//    }
-//
-//    @ShellMethod(key="get-sorted",value="Get sorted Entities")
-//    public void getSortedEntities(){
-//        sorter.getSortedRepo();
-//    }
-//
-//    @ShellMethod(key = "pick-sorter", value = "Pick sorter")
-//    public void pickSorter(@ShellOption(help = "Specify the sorting option") String option) {
-//        switch (option) {
-//            case "departments":
-//                // Handle the --departments option
-//                setSortRepo(depsort);
-//                System.out.println("Sorting departments");
-//                break;
-//            case "employees":
-//                // Handle the --employees option
-//                System.out.println("Sorting employees");
-//                setSortRepo(empsort);
-//                break;
-//            default:
-//                System.out.println("Invalid option: " + option);
-//                // Handle the case where an unknown option is provided
-//                break;
-//        }
-//    }
-//
-//
-//}
+package map.project.MihaiStupyMAPSpring.data.sorterLogic;
+
+import map.project.MihaiStupyMAPSpring.data.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+import org.springframework.stereotype.Component;
+
+@ShellComponent
+@Component
+public class SorterCommands {
+    //@Autowired
+    private Sorter sorter;
+
+    @Autowired
+    private DepartmentSorter depsort;
+    @Autowired
+    private EmployeeSorter empsort;
+
+
+   /// @ShellMethod(key="set-repo",value="Set sorting repo")
+    public void setSortRepo(Sorter sorter){
+        this.sorter = sorter;
+    }
+
+    @ShellMethod(key="get-sorted",value="Get sorted Entities")
+    public void getSortedEntities(){
+        sorter.getSortedRepo();
+    }
+
+    @ShellMethod(key = "pick-sorter", value = "Pick sorter")
+    public void pickSorter(@ShellOption(help = "Specify the sorting option") String option) {
+        switch (option) {
+            case "departments":
+                // Handle the --departments option
+                setSortRepo(depsort);
+                System.out.println("Sorting departments");
+                break;
+            case "employees":
+                // Handle the --employees option
+                System.out.println("Sorting employees");
+                setSortRepo(empsort);
+                break;
+            default:
+                System.out.println("Invalid option: " + option);
+                // Handle the case where an unknown option is provided
+                break;
+        }
+    }
+
+
+}
